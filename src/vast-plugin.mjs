a4de3c2ea33824a7637537d3e3f574de0ca7d72f
Ingo Oppermann <ingo@oppermann.ch>
2025-09-09 16:19:22 +0200

    Revert commit again

--- src/vast-plugin.mjs
+++ src/vast-plugin.mjs
@@ -238,7 +238,7 @@ export class VastPlugin extends Plugin {
         });
 
         // TODO: calculate reasonable timeout based on contrib-ads settings
-        setTimeout(signalAdsReady, 3000);
+        setTimeout(signalAdsReady, 100);
 
         const adLoader = new AdLoader(vastClient, new VASTParser(), new AdSelector(), options);
         adLoader
